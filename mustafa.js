a4acb986e9bf6c82c7aa2a6437cf426b65280481
Mustafa Lashuel <135173315+MustafaLas@users.noreply.github.com>
2023-12-24 21:44:29 -0800

    Home page tweaks about section pprojects section

--- mustafa.js
+++ mustafa.js
@@ -6,3 +6,9 @@ function darkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+
+document.getElementById("coolHeader").addEventListener("keydown", function (event) {
+  // Prevent editing of the text
+  event.preventDefault();
+});
